e3c6e70942d40b69feef25707f61385747171d2d
Randy <Randy@ecinternet.com>
2015-07-09 21:46:00 -0700

    Began project, completed problem 1.

---
+++ ProjectEuler/ProjectEuler/Problems/Problem1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+	class Problem1
+	{
+		public static void Run()
+		{
+			int MAX = 1000;
+			int count = 0;
+
+			for (int i = 1; i < MAX; i++)
+			{
+				if (i%3==0 || i%5==0)
+				{
+					count += i;
+				}
+			}
+
+			Console.WriteLine(count);
+		}
+	}
+}
